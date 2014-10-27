f554b74c8568bda15f78431e02e3f59ab11caccb
maximuschang <gn00982591@msn.com>
2014-10-27 23:01:15 +0800

    20141027007

---
+++ MaxiMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaxiMvc.MvcApplication" Language="C#" %>
---
+++ MaxiMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaxiMvc.Startup))]
+namespace MaxiMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
