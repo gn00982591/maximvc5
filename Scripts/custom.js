e12da6aa489498efc94037baf35f3ec9ce2f902b
maximuschang <gn00982591@msn.com>
2015-01-01 11:46:42 +0800

    20150101001

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -13,14 +13,14 @@
         /* 
         * Animation scroll
         */
-        $('a[href*=#]').bind('click', function (e) {
-            var anchor = $(this);
+        //$('a[href*=#]').bind('click', function (e) {
+        //    var anchor = $(this);
 
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
-            }, 500);
-            e.preventDefault();
-        });
+        //    $('html, body').stop().animate({
+        //        scrollTop: $(anchor.attr('href')).offset().top
+        //    }, 500);
+        //    e.preventDefault();
+        //});
 
         /*
         * Background slideshow
