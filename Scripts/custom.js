3c59746b9a6029fcb0a9074680a869f5b7ee2b09
gn00982591 <gn00982591@msn.com>
2015-01-07 17:46:37 +0800

    20150107001

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -3,6 +3,7 @@
     * Preloader 
     */
     $(window).load(function () {
+
         $('#status').fadeOut();
         $('#preloader').delay(350).fadeOut('slow');
         $('body').delay(350).css({ 'overflow': 'visible' });
